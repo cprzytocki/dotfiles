2f6fdf08617bccbd665df3d29cdcbcffd5dab3d1
Chris Przytocki <cprzytocki@gmail.com>
2025-08-23 12:12:18 -0400

    wezterm root

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,9 +7,9 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 config.color_scheme = 'Catppuccin Mocha'
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 10
+config.font_size = 14
 config.enable_tab_bar = true
--- config.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
 config.enable_wayland = true
@@ -20,19 +20,16 @@ config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.show_new_tab_button_in_tab_bar = false
 
--- scroll amount doesnt work wtf
--- alternate_buffer_wheel_scroll_speed = 1
-
 local action = wezterm.action
 config.keys = {
     -- naturtal text editting
 	{ mods = "OPT", key = "LeftArrow", action = action.SendKey({ mods = "ALT", key = "b" }) },
 	{ mods = "OPT", key = "RightArrow", action = action.SendKey({ mods = "ALT", key = "f" }) },
-	-- change to "CMD" for macos
-	{ mods = "CTRL", key = "LeftArrow", action = action.SendKey({ mods = "CTRL", key = "a" }) },
-	{ mods = "CTRL", key = "RightArrow", action = action.SendKey({ mods = "CTRL", key = "e" }) },
-	{ mods = "CTRL", key = "Backspace", action = action.SendKey({ mods = "CTRL", key = "u" }) },
-	{ mods = 'CTRL', key = 'k',  action = action.ClearScrollback('ScrollbackAndViewport') },
+	{ mods = "CMD", key = "LeftArrow", action = action.SendKey({ mods = "CTRL", key = "a" }) },
+	{ mods = "CMD", key = "RightArrow", action = action.SendKey({ mods = "CTRL", key = "e" }) },
+	{ mods = "CMD", key = "Backspace", action = action.SendKey({ mods = "CTRL", key = "u" }) },
+	{ mods = 'CMD', key = 'k',  action = action.ClearScrollback('ScrollbackAndViewport') },
+
 }
 -- and finally, return the configuration to wezterm
 return config
